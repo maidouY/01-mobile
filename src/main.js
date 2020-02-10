c6e5a6b8badaed99b4fd030a241d6428c658035f
麦兜儿 <807067838@qq.com>
2020-02-10 10:24:34 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 // import Vue from 'vue'
 import Vue from 'vue'
 import App from './App.vue'
-
-// 导入插件文件
-import MyPlugin from './MyPlugin.js'
-// 注册插件
-Vue.use(MyPlugin) // 本质：执行install方法而已
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
